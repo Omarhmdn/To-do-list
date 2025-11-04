0c84b176f10added23d23bbcd3136f6963dfc2a4
Lucas Parbott Clowe <cmnp0658@leeds.ac.uk>
2025-11-04 12:32:58 +0000

    Mostly finished add task

--- program.py
+++ program.py
@@ -1,2 +1,6 @@
-def add_function():
-    
+def add_task(Current_List):
+    task = input("Please enter the task you would like to add: ").strip().lower()
+    due_date = input("Please enter the due date of the task in the form dd/mm/yyyy: ").strip()
+    Current_List.append([task, due_date, False])
+    print(f'"{task}" has been successfully added to the list!')
+    return Current_List
