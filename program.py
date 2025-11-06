c2e440a8f4ef8d2082c9598692b9916263c45b2c
Omarhmdn <jcsk0685@leeds.ac.uk>
2025-11-06 16:40:55 +0000

    final program.py

--- program.py
+++ program.py
@@ -126,3 +126,14 @@ def save_list(file_name, Current_List, lists_path):
         t.sleep(1)
         print(".")
     print("\nList has been saved!\n")
+
+def delete_list():
+    f = input("File name: ")
+    try:
+        t = open(f).read().splitlines()
+        for i, x in enumerate(t): print(i+1, x)
+        n = int(input("Task number to delete: ")) - 1
+        t.pop(n)
+        open(f, "w").write("\n".join(t))
+        print("Task deleted!")
+    except: print("Error")
