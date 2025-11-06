9b199afdfe4b98fc367cd07ac8e888dfba0b82e7
Omarhmdn <jcsk0685@leeds.ac.uk>
2025-11-06 16:30:32 +0000

    added display_list()

--- program.py
+++ program.py
@@ -74,3 +74,11 @@ def create_or_add_to_existing_list(list_loaded, Current_List, file_name, lists_p
         else:
             running = False
             save_list(file_name, Current_List, lists_path)
+
+def display_list(Current_List):
+    if not Current_List:
+        print("The list is currently empty.")
+    else:
+        print("\nYour current tasks:")
+        for i, task in enumerate(Current_List, start=1):
+            print(f"{i}. {task}")
